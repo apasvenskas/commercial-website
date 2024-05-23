a3889ce6dd9310199e308705b473df5a30052b40
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-05-22 23:50:30 -0700

    data called to menu list

--- components/menuList/dumyList.js
+++ components/menuList/dumyList.js
@@ -1,13 +1,24 @@
 import styles from './dumyList.module.css'
+import Link from 'next/link'
 
 export default function DumyList(){
     return(
         <div className={styles.categories}>
+            <Link href="/">
             <p className={styles.p}>Still Object</p>
+            </Link>
+            <Link href="/">
             <p className={styles.p}>Abstract</p>
+            </Link>
+            <Link href="/">
             <p className={styles.p}>Portret</p>
+            </Link>
+            <Link href="/">
             <p className={styles.p}>Landscape</p>
+            </Link>
+            <Link href="/">
             <p className={styles.p}>Allegory</p>
+            </Link>
         </div>
     )
 }
--- components/menuList/menuList.js
+++ components/menuList/menuList.js
@@ -1,7 +1,40 @@
+import { GraphQLClient, gql } from "graphql-request";
 import DumyList from "./dumyList";
 import styles from "./menuList.module.css";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const graphcms = new GraphQLClient(process.env.
+  GRAPHCMS_ENDPOINT, {
+    headers: {
+    Authorization: `Baerer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+})
+
+const query = gql `
+query MyQuery {
+  paintings {
+    artist
+    id
+    title
+    type
+  }
+}
+`
+
 
 export default function MenuList() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    console.log(" menu data is", data);
+    async function getMenuItems(){
+      const data = await graphcms.request(query)
+      setData(data)
+    }
+    getMenuItems()
+  }, [])
+
   return (
     <div className={styles.leftMenu}>
         <div className={styles.menu}>
