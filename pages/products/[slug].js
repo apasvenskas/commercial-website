e89ab236c779fff4503cd33fa7d0456eb89be6f1
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-06-03 18:21:38 -0700

    data pased to the slug successfully

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,8 +1,78 @@
+import { GraphQLClient, gql } from "graphql-request";
+import useGetPaintingDetails from "@/utils/useGetPainitngsDetails";
 
-export default function SlugPage(){
+const hygraph = new GraphQLClient(process.env.NEXT_PUBLIC_HYGRAPH_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_TOKEN}`,
+    },
+  });
+
+export default function SlugPage({product}){
+
+    const productArray = Object.values(product);
+    let item = {};
+    productArray.map(items => {
+        items.map(i => {
+            item = i;
+        });
+    });
+
+    const {
+        isNewProduct,
+        isPromoProd,
+        price,
+        tempPrice,
+        discount,
+        discountPrice,
+        imgSrc,
+        mainImgSrc,
+        id,
+        title,
+      } = useGetPaintingDetails(item);
+
+    console.log('slug item is', item)
+
     return(
         <div>
             Slug
         </div>
     )
+}
+
+export async function getServerSideProps(context){
+
+    const currentSlug = context.params.slug
+
+    const query = gql`
+    query ($currentSlug: String!){
+        paintings(where: {slug: $currentSlug}) {
+          artist
+          id
+          price
+          promotion
+          paintingSurface
+          newProduct
+          slug
+          stock
+          title
+          type
+          subtitle
+          images {
+            url
+          }
+          description {
+            raw
+          }
+        }
+      }
+    `;
+
+    const variables = {currentSlug}
+    const product = await hygraph.request(query, variables)
+
+    return{
+        props: {
+            product: product,
+        }
+    }
 }
