90a6d00b2bf935c396f4732ac2c8531c93dd0d8d
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-06-10 18:12:00 -0700

    map added to the contacts

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -51,18 +51,44 @@ export default function Contacts() {
                 <label className={styles.label}>Subject:</label>
                 <input className={styles.input} type="text" name="subject" />
                 <label className={styles.label}>Message:</label>
-                <textarea name="message" className={styles.messageBox}/>
+                <textarea name="message" className={styles.messageBox} />
                 <input className={styles.button} type="submit" value="Send" />
               </form>
               <div className={styles.infoSection}>
                 <div className={styles.infoCard}>
                   <h4 className={styles.cardTitle}>Contact Info</h4>
-                  <h4 className={styles.cardDispaly}><span className={styles.contactInfo}>Name:  </span>	Aušra Martinkutė-Juodienė</h4>
-                  <h4 className={styles.cardDispaly}><span className={styles.contactInfo}>Telephone:  </span>+370 620 64677</h4>
-                  <h4 className={styles.cardDispaly}><span className={styles.contactInfo}>Email:  </span>kynas42@gmail.com</h4>
-                  <h4 className={styles.cardDispaly}><span className={styles.contactInfo}>Address:  </span>Rotušės a. 1-15, LT-44280 Kaunas</h4>
-                  <h4 className={styles.cardDispaly}><span className={styles.contactInfo}>Location:  </span>Kaunas, Lithuania</h4>
+                  <h4 className={styles.cardDispaly}>
+                    <span className={styles.contactInfo}>Name: </span> Aušra
+                    Martinkutė-Juodienė
+                  </h4>
+                  <h4 className={styles.cardDispaly}>
+                    <span className={styles.contactInfo}>Telephone: </span>+370
+                    620 64677
+                  </h4>
+                  <h4 className={styles.cardDispaly}>
+                    <span className={styles.contactInfo}>Email: </span>
+                    kynas42@gmail.com
+                  </h4>
+                  <h4 className={styles.cardDispaly}>
+                    <span className={styles.contactInfo}>Address: </span>Rotušės
+                    a. 1-15, LT-44280 Kaunas
+                  </h4>
+                  <h4 className={styles.cardDispaly}>
+                    <span className={styles.contactInfo}>Location: </span>
+                    Kaunas, Lithuania
+                  </h4>
                 </div>
+              </div>
+            </div>
+            <div className={styles.mapSection}>
+              <div className={styles.map}>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2294.342133397457!2d23.88539907556076!3d54.896916957588594!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e7220ff3184881%3A0x8002872ac9bbdb7c!2sRotu%C5%A1%C4%97s%20a.%201%2C%20Kaunas%2C%2044280%20Kauno%20m.%20sav.%2C%20Lithuania!5e0!3m2!1sen!2sus!4v1718066267243!5m2!1sen!2sus"
+                  style={{ width: "100%", height: "100%", border: 0 }}
+                  allowFullScreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
               </div>
             </div>
           </div>
@@ -70,4 +96,4 @@ export default function Contacts() {
       </div>
     </>
   );
-}  
+}
