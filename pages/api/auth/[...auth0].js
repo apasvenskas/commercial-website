f74cdc962d0864abb4315002d4eca9f280dce4ce
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-08-11 22:54:55 -0700

    changes to auth file for loging the errors

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,41 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin, handleLogout, handleCallback, handleProfile } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    try {
+      console.log('Logging in...');
+      await handleLogin(req, res);
+    } catch (error) {
+      console.error('Login error:', error);
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+  async callback(req, res) {
+    try {
+      console.log('Handling callback...');
+      await handleCallback(req, res);
+      console.log('Callback successful');
+    } catch (error) {
+      console.error('Callback error (Token exchange failed):', error);
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+  async logout(req, res) {
+    try {
+      console.log('Logging out...');
+      await handleLogout(req, res);
+    } catch (error) {
+      console.error('Logout error:', error);
+      res.status(error.status || 500).end(error.message);
+    }
+  },
+  async profile(req, res) {
+    try {
+      console.log('Fetching profile...');
+      await handleProfile(req, res);
+    } catch (error) {
+      console.error('Profile fetch error:', error);
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+});
