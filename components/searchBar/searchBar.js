b0ff82ca6ca2b3af74f9f50a298c5f74ea013850
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-09-22 21:03:28 -0700

    added handleKeyDown for enter in searchbar

--- components/searchBar/searchBar.js
+++ components/searchBar/searchBar.js
@@ -16,11 +16,24 @@ export default function SearchBar({ onSearch, allPaintings }) {
     onSearch(filtered);
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      const filtered = allPaintings.filter(painting =>
+        painting.title.toLowerCase().includes(query) ||
+        painting.subtitle.toLowerCase().includes(query)
+      );
+      
+      onSearch(filtered);
+    }
+  };
+
+
   return (
     <input
       type="text"
       value={query}
       onChange={handleSearch}
+      onKeyDown={handleKeyDown}
       placeholder="Search by title or type..."
       className={styles.searchInput}
     />
